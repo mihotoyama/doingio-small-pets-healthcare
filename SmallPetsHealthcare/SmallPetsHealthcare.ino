4898b131da6d78dc4a82fc127b87d15ffa729c63
Miho Toyama <tourspoitiers@gmail.com>
2021-12-16 20:25:13 +0900

    JSON形式で計測データをPOST送信できるようにコード追加

--- SmallPetsHealthcare/SmallPetsHealthcare.ino
+++ SmallPetsHealthcare/SmallPetsHealthcare.ino
@@ -1,3 +1,6 @@
+#include <HttpClient.h>
+#include <ArduinoJson.h>
+
 #include <M5Stack.h>
 #include <WiFi.h>
 #include <time.h>
@@ -24,16 +27,12 @@ DayDataModel nowDayData;
 DatetimeController datetimeController;
 UIViewController uiViewController;
 
-
-
 //センサークラスをインスタンス化
 TempHumController tempHumController;
 WeightController weightController;
 TOFCountController houseCountController;
 DICountController wheelCountController;
 MotionCountController ActiveCountController;
-
-
 
 //外部サービス連携クラスをインスタンス化
 AmbentController ambientContrller;
@@ -45,7 +44,8 @@ int beforeMin = -1;
 
 WiFiClient client;
 
-void setup() {
+void setup()
+{
   M5.begin();
   Wire.begin();
 
@@ -61,16 +61,17 @@ void setup() {
   Serial.println("wifi start");
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWD);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(100);
   }
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     M5.Lcd.println("Connected");
     M5.Lcd.println(WiFi.localIP());
-    
+
     Serial.println("Connected");
     Serial.println(WiFi.localIP());
-
   }
 
   Serial.println("setup start");
@@ -88,7 +89,6 @@ void setup() {
   //再起動やクラッシュに備えて保存したtempデータを読み込み
   loadTempData();
 
-
   //--センサークラスをセットアップ
   weightController.setup(&nowDayData, CH1, WEIGHT_DOUT_PIN, WEIGHT_SLK_PIN, WEIGHT_THRESHOLD, WEIGHT_OUTVOL, WEIGHT_LOAD, WEIGHT_CALIB);
   ActiveCountController.setup(&nowDayData, CH2, MOTION_PIN);
@@ -96,59 +96,75 @@ void setup() {
   wheelCountController.setup(&nowDayData, CH4, WHEEL_PIN1, WHEEL_PIN2, WHEEL_TIMECHAT);
   houseCountController.setup(&nowDayData, CH5, HOME_THRESHOLD_MIN, HOME_THRESHOLD_MAX);
 
-
   //外部連携クラスをセットアップ
   ambientContrller.setup(&nowDayData, AMBIDATA_CH_ID, AMBIDATA_WRITEKEY, &client);
-  twitterController.setup(&nowDayData, TW_TOKEN); 
-    
+  twitterController.setup(&nowDayData, TW_TOKEN);
 }
 
-
-
-void loop() {
+void loop()
+{
   m5.update();
 
   //ベースクラスをアップデート
   datetimeController.update();
 
-
   //--センサークラスをアップデート
   weightController.update();
   ActiveCountController.update();
-  wheelCountController.update(); 
-  
+  wheelCountController.update();
+
   paHUB_selectChannel(3); //PaHUBでI2Cを切り替え
   houseCountController.update();
-  
+
   paHUB_selectChannel(5); //PaHUBでI2Cを切り替え
   tempHumController.update();
-
-
-
 
   //UIをアップデート＆描画
   uiViewController.update();
   uiViewController.draw();
 
+  //データをjson形式でサーバーに送信
+  int displayYear = nowDayData.nowtimeInfo.tm_year + 1900;
+  int displayMonth = nowDayData.nowtimeInfo.tm_mon + 1;
+  int displayDay = nowDayData.nowtimeInfo.tm_mday;
 
+  char buf[128];
+  memset(buf, 0x00, sizeof(buf));
+  sprintf(buf, "%04d%02d%02d%02d", displayYear, displayMonth, displayDay);
+
+  //データ送信用jsonの用意
+  StaticJsonDocument<255> json_request;
+  char buffer[255];
+  memset(buffer, 0x00, sizeof(buffer));
+  json_request["nowtime"] = buf;
+  json_request["weight"] = nowDayData.sensorsValue[0];
+  json_request["activeCount"] = nowDayData.sensorsValue[1];
+  json_request["temperature"] = nowDayData.sensorsValue[2];
+  json_request["wheelCount"] = nowDayData.sensorsValue[3];
+  json_request["houseCount"] = nowDayData.sensorsValue[4];
+  json_request["humidity"] = nowDayData.sensorsValue[5];
+
+  serializeJson(json_request, buffer, sizeof(buffer));
+
+  sendJson(buffer);
 
   //１分おきに実行
-  if(beforeMin!=nowDayData.nowtimeInfo.tm_min)
+  if (beforeMin != nowDayData.nowtimeInfo.tm_min)
   {
     beforeMin = nowDayData.nowtimeInfo.tm_min;
     saveTempData(); //再起動、クラッシュに備えて定期的に値をバックアップ(temp)として保存
   }
 
   //１時間おきに実行
-  if(beforeHour!=nowDayData.nowtimeInfo.tm_hour)
+  if (beforeHour != nowDayData.nowtimeInfo.tm_hour)
   {
     beforeHour = nowDayData.nowtimeInfo.tm_hour;
     ambientContrller.send();
     twitterController.tweet();
   }
-  
+
   //１日おきに実行
-  if(beforeDay!=nowDayData.nowtimeInfo.tm_mday)
+  if (beforeDay != nowDayData.nowtimeInfo.tm_mday)
   {
     beforeDay = nowDayData.nowtimeInfo.tm_mday;
 
@@ -164,16 +180,14 @@ void loop() {
     uiViewController.reload();
   }
 
-
   delay(1);
 }
-
 
 void paHUB_selectChannel(int ch)
 {
   //PaHUBでI2Cを切り替え
   baHUB.address(0x70);
-  baHUB.selectChannel(ch); 
+  baHUB.selectChannel(ch);
 }
 
 void loadTempData()
@@ -182,73 +196,96 @@ void loadTempData()
   CSV_Parser cp("sffffff");
 
   File file = SD.open(TEMPDATA_PATH);
-  if (file) {
-      
-      while(file.available()) {
-          cp << file.read();
-      }
-      file.close();
+  if (file)
+  {
 
-      int len = cp.getRowsCount();
+    while (file.available())
+    {
+      cp << file.read();
+    }
+    file.close();
 
-      Serial.print("temp len: ");    
-      Serial.println(len);
+    int len = cp.getRowsCount();
 
-      int c = 0;
-      for(int i = len-1; i >= 0; i--)
+    Serial.print("temp len: ");
+    Serial.println(len);
+
+    int c = 0;
+    for (int i = len - 1; i >= 0; i--)
+    {
+      char str[16];
+      strcpy(str, ((char **)cp[0])[i]);
+      char *dst[8];
+      int count = split(dst, str, '/');
+      if (count < 3)
       {
-          char str[16];
-          strcpy(str, ((char**)cp[0])[i]);
-          char *dst[8];
-          int count = split(dst, str, '/');
-          if(count < 3)
-          {
-              Serial.print("date error: ");
-              Serial.println(str);
-              continue; //日付フォーマットではない
-          } 
-
-          struct tm  tinfo;
-          memset(&tinfo, 0, sizeof(struct tm));
-          tinfo.tm_year = (atoi(dst[0]) - 1900);
-          tinfo.tm_mon = (atoi(dst[1]) - 1);
-          tinfo.tm_mday = (atoi(dst[2]));
-
-
-          nowDayData.nowtimeInfo = tinfo;
-          nowDayData.sensorsValue[0] = ((float*)cp[1])[i];
-          nowDayData.sensorsValue[1] = ((float*)cp[2])[i];
-          nowDayData.sensorsValue[2] = ((float*)cp[3])[i];
-          nowDayData.sensorsValue[3] = ((float*)cp[4])[i];
-          nowDayData.sensorsValue[4] = ((float*)cp[5])[i];
-          nowDayData.sensorsValue[5] = ((float*)cp[6])[i];
-          c++;
-
-          if(c >=1) break;
+        Serial.print("date error: ");
+        Serial.println(str);
+        continue; //日付フォーマットではない
       }
+
+      struct tm tinfo;
+      memset(&tinfo, 0, sizeof(struct tm));
+      tinfo.tm_year = (atoi(dst[0]) - 1900);
+      tinfo.tm_mon = (atoi(dst[1]) - 1);
+      tinfo.tm_mday = (atoi(dst[2]));
+
+      nowDayData.nowtimeInfo = tinfo;
+      nowDayData.sensorsValue[0] = ((float *)cp[1])[i];
+      nowDayData.sensorsValue[1] = ((float *)cp[2])[i];
+      nowDayData.sensorsValue[2] = ((float *)cp[3])[i];
+      c++;
+
+      if (c >= 1)
+        break;
+    }
   }
 }
 void saveTempData()
 {
-    char buf[128];
-    sprintf(buf, "%04d/%02d/%02d,", nowDayData.nowtimeInfo.tm_year+1900, nowDayData.nowtimeInfo.tm_mon+1, nowDayData.nowtimeInfo.tm_mday);
-    String str = buf;
+  char buf[128];
+  sprintf(buf, "%04d/%02d/%02d,", nowDayData.nowtimeInfo.tm_year + 1900, nowDayData.nowtimeInfo.tm_mon + 1, nowDayData.nowtimeInfo.tm_mday);
+  String str = buf;
 
-    for(int i = 0; i < 6; i++) {
-        str += nowDayData.sensorsValue[i];
-        if(i<(6-1))
-        {
-            str += ",";
-        }
+  for (int i = 0; i < 6; i++)
+  {
+    str += nowDayData.sensorsValue[i];
+    if (i < (6 - 1))
+    {
+      str += ",";
     }
-    
-    str = "DATE,CH1,CH2,CH3,CH4,CH5,CH6\n" + str;
-  
-    Serial.print("saveTemp: ");
-    Serial.println(str);
+  }
 
-    File file = SD.open(TEMPDATA_PATH, FILE_WRITE);
-    file.println(str);
-    file.close();
+  str = "DATE,CH1,CH2,CH3,CH4,CH5,CH6\n" + str;
 
+  Serial.print("saveTemp: ");
+  Serial.println(str);
+
+  File file = SD.open(TEMPDATA_PATH, FILE_WRITE);
+  file.println(str);
+  file.close();
+}
+
+void sendJson(char *buffer)
+{
+  Serial.println("Json to send:");
+  Serial.println(buffer);
+
+  HTTPClient http;
+  http.begin(HOST);
+  http.addHeader("Content-Type", "application/json");
+  int status_code = http.POST((uint8_t *)buffer, strlen(buffer));
+  Serial.printf("status_code=%d\r\n", status_code);
+
+  if (status_code == 201)
+  {
+    Stream *resp = http.getStreamPtr();
+
+    DynamicJsonDocument json_response(255);
+    deserializeJson(json_response, *resp);
+
+    serializeJson(json_response, Serial);
+    Serial.println("");
+  }
+  http.end();
 }
